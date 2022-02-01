408f2fc1d4158629fd2e9d47e4d29060bfcbefe5
TheLuisAbarca <arclaav.adm@gmail.com>
2022-02-01 16:54:14 -0500

    Add basic structure for routes on app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import {
+  Link, Route, Routes, useLocation,
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navbar } from 'react-bootstrap';
+import Details from './components/Details/Details';
+import Home from './components/Home/Home';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
-    <div className="App" />
+    <>
+      <header>
+        <Navbar className="px-4 text-white bg-black d-flex justify-content-between">
+          <Link to="/" className="text-decoration-none text-white fw-bold">
+            &#60; HOME
+          </Link>
+          <h3 className="m-0">PokeStats</h3>
+          <span>
+            <i className="me-4 fas fa-microphone" />
+            <i className="fas fa-cog" />
+          </span>
+        </Navbar>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="details/:name" element={<Details />} />
+        </Routes>
+      </main>
+    </>
   );
-}
+};
 
 export default App;
