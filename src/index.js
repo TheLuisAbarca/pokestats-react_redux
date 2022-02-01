e22bda9a3f089faafcead11be13038fb45e6c98e
TheLuisAbarca <arclaav.adm@gmail.com>
2022-02-01 16:52:13 -0500

    Add estructure to use the store globally in the  application.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './redux/configureStore';
+import { fetchPokemons } from './redux/filter/filter';
 import './index.css';
 import App from './App';
 
+store.dispatch(fetchPokemons('normal'));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
